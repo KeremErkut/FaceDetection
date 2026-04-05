944fdc3ff00ae52e5ab914ac1ee8d7319c5d6fa5
KeremErkut <kciftlikci@gmail.com>
2026-04-05 20:02:40 +0300

    feat: add BitmapExtensions for Mat to BitmapSource conversion

---
+++ FaceDetection/Helpers/BitmapExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using OpenCvSharp;
+
+namespace FaceDetection.Helpers
+{
+    public static class BitmapExtensions
+    {
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        /// <summary>
+        /// Converts an OpenCV Mat to a WPF compatible BitmapSource.
+        /// This is an extension method for ease of use.
+        /// </summary>
+        public static BitmapSource ToBitmapSource(this Mat image)
+        {
+            if (image == null || image.IsDisposed) return null;
+
+            // Using the OpenCvSharp helper for conversion
+            return OpenCvSharp.WpfExtensions.BitmapSourceConverter.ToBitmapSource(image);
+        }
+    }
+}
