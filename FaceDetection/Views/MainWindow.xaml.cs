a887b25285663b0728ed900ea5b7952cdb1196b3
KeremErkut <kciftlikci@gmail.com>
2026-04-05 20:21:41 +0300

    feat: create WPF UI and finalize MVVM data binding

---
+++ FaceDetection/Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using FaceDetection.ViewModels;
+
+namespace FaceDetection.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// Following MVVM, this class only handles View initialization and DataContext binding.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set the DataContext to our ViewModel to enable Data Binding
+            // This links our UI elements to the logic in MainViewModel
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
